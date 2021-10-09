912b425a68dd3a7b8d0f66ea00c0b6c10f89d1ba
YoungStash <mixa.kyl@mail.ru>
2021-10-09 17:22:39 +0300

    Done

---
+++ config.py
@@ -0,0 +1,21 @@
+# Configuration
+
+DEBUG = False
+
+# training
+EPOCHS = 40
+MOMENTUM = 0.9
+LEARNING_RATE = 0.01
+BATCH_SIZE = 8
+THREADS = 8
+
+USE_CUDA=True
+
+# file paths
+IMAGES_PATH = "new_dataset"
+TRAINING_PATH = "train_file.csv"
+VALIDATION_PATH = "test_file.csv"
+TEST_PATH = "test_file.csv"
+WEIGHTS_PATH = "result_weights/epoch_30.pt"
+
+RESULTS_PATH="results"
